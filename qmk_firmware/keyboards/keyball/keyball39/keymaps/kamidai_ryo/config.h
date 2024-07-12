f24e2c12fbbb827fda4af11a8cff4d3755925744
Aress1988 <s_ryo_tarou1_8@yahoo.co.jp>
2024-07-12 21:20:45 +0900

    keymap バグ修正/lib　マウス  調整)

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_ryo/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_ryo/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
-#define KEYBALL_CPI_DEFAULT 100       // 光学センサーPMW3360DM の解像度 (CPI) の規定値
+#define KEYBALL_CPI_DEFAULT 200       // 光学センサーPMW3360DM の解像度 (CPI) の規定値
 #define KEYBALL_SCROLL_DIV_DEFAULT 6  // スクロール速度の規定値
 
 // ファームウェアのサイズを削減
