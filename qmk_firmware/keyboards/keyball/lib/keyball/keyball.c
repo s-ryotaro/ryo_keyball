361781fdb38bc889e9d816f64f03a167b1ec4376
Aress1988 <s_ryo_tarou1_8@yahoo.co.jp>
2024-07-13 21:25:06 +0900

    libマウス調整（＞記号が間違えていたのを修正）

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -173,11 +173,11 @@ static void adjust_mouse_speed(keyball_motion_t *m){
   float speed_multiplier = 1.0; //速度の倍率
   if (movement_size > 30) {
     speed_multiplier = 7.0;//5
-  } else if(movement_size < 20){
+  } else if(movement_size > 20){
     speed_multiplier = 4.5;//4.5
-  } else if(movement_size < 15){
+  } else if(movement_size > 15){
     speed_multiplier = 4.0;//4
-  } else if(movement_size < 10){
+  } else if(movement_size > 10){
     speed_multiplier = 2.5;//3
   } else if(movement_size > 7){
     speed_multiplier = 1.8;//2.5
