2b8cbae30ba9a7a7c4a5992ed0557e679915ed09
Aress1988 <s_ryo_tarou1_8@yahoo.co.jp>
2024-07-31 08:00:17 +0900

    日本語キーエイリアスNG 直接ずれているキーを探す＿ 修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_ryo/features/tap_dance.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai_ryo/features/tap_dance.h
@@ -3,6 +3,15 @@
 
 #include "quantum_keycodes.h"
 
+// Define missing keycodes if not already defined
+#ifndef KC_RBRC
+#define KC_RBRC 0x1E  // Example value for right bracket key
+#endif
+
+#ifndef KC_BSLS
+#define KC_BSLS 0x2B  // Example value for backslash key
+#endif
+
 // Tap Dance declarations
 enum tap_dance{
   TD_GUI_D,
@@ -51,13 +60,13 @@ void dance_brackets_finished(tap_dance_state_t *state, void *user_data) {
             break;
         case 2:
             // （）
-            send_tap_code_with_left_arrow(LSFT(KC_9));
-            send_tap_code_with_left_arrow(LSFT(KC_0));
+            send_tap_code_with_left_arrow(S(KC_9));
+            send_tap_code_with_left_arrow(S(KC_0));
             break;
         case 3:
             // {}
-            send_tap_code_with_left_arrow(LSFT(RBRC));
-            send_tap_code_with_left_arrow(LSFT(BSLS));
+            send_tap_code_with_left_arrow(S(KC_RBRC));
+            send_tap_code_with_left_arrow(S(KC_BSLS));
             break;
     }
     reset_tap_dance(state);
@@ -74,7 +83,7 @@ void dance_kana_symbols_finished(tap_dance_state_t *state, void *user_data) {
             tap_code16(KC_EXLM);  // "！"
             break;
         case 3:
-            tap_code16(LSFT(KC_EQL));  // "～"※ひらがな入力時
+            tap_code16(S(KC_EQL));  // "～"※ひらがな入力時
             break;
     }
     reset_tap_dance(state);
@@ -97,10 +106,10 @@ void dance_other_symbols_finished(tap_dance_state_t *state, void *user_data) {
             tap_code16(KC_HASH);  // "#"
             break;
         case 5:
-            tap_code16(LSFT(KC_7));  // "&"
+            tap_code16(S(KC_7));  // "&"
             break;
         case 6:
-            tap_code16(LSFT(KC_4));  // "$"
+            tap_code16(S(KC_4));  // "$"
             break;
     }
     reset_tap_dance(state);
@@ -115,10 +124,10 @@ void dance_other_Q_symbols_2_finished(tap_dance_state_t *state, void *user_data)
             tap_code(KC_Q);
             break;
         case 2:   // 2回タップで中点（・）※ひらがな入力時
-            tap_code(LSFT(KC_SLSH));
+            tap_code(S(KC_SLSH));
             break;
         case 3:   // 3回タップで縦棒（｜）   
-            tap_code(LSFT(KC_INT3));
+            tap_code(S(KC_INT3));
             break;
         case 4:    // 4回タップで「\」  
             tap_code(KC_INT1);
@@ -142,14 +151,14 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_MINUS_TO_UNDUNDER] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS,S(KC_INT1)),
 
     //記号 計算
-    [TD_PLUS_MAINUS]= ACTION_TAP_DANCE_DOUBLE(LSFT(KC_SCLN),KC_MINS),         //  +  -    LSFT(KC_EQL)
-    [TD_MULTI_DIVID]= ACTION_TAP_DANCE_DOUBLE(LSFT(KC_EQL),KC_SLSH),          //  *  /
-    [TD_EQUAL_CARET]= ACTION_TAP_DANCE_DOUBLE(LSFT(KC_MINS),KC_EQL),          //  =  ^    (KC_EQL,LSFT(KC_6)), 
+    [TD_PLUS_MAINUS]= ACTION_TAP_DANCE_DOUBLE(S(KC_SCLN),KC_MINS),         //  +  -    LSFT(KC_EQL)
+    [TD_MULTI_DIVID]= ACTION_TAP_DANCE_DOUBLE(S(KC_EQL),KC_SLSH),          //  *  /
+    [TD_EQUAL_CARET]= ACTION_TAP_DANCE_DOUBLE(S(KC_MINS),KC_EQL),          //  =  ^    (KC_EQL,LSFT(KC_6)), 
     
     //記号 その他
     [TD_COLON_SEMICOLON]= ACTION_TAP_DANCE_DOUBLE(KC_QUOT,KC_SCLN),           //  :  ;
-    [TD_FUTOUGOU]= ACTION_TAP_DANCE_DOUBLE(LSFT(KC_COMM),LSFT(KC_DOT)),       //  <  >
-    [TD_QUOTATION]= ACTION_TAP_DANCE_DOUBLE(KC_QUOT,LSFT(KC_QUOT)),           //  '  "
+    [TD_FUTOUGOU]= ACTION_TAP_DANCE_DOUBLE(S(KC_COMM),S(KC_DOT)),       //  <  >
+    [TD_QUOTATION]= ACTION_TAP_DANCE_DOUBLE(KC_QUOT,S(KC_QUOT)),           //  '  "
 
     //▼3回以上タップ
     [TD_BRACKETS] = ACTION_TAP_DANCE_FN(dance_brackets_finished),
